b310ab59dbda699855cb52417f050c29fa065d43
EPCTechnologies <weexianbin@yahoo.com.sg>
2021-06-11 01:27:55 +0100

    Add edgelist, restricted to only transmission network assets

--- power_osm.py
+++ power_osm.py
@@ -17,6 +17,9 @@ def load_power_osm_data():
         point = pd.read_pickle("data/power_osm/in/power_OSM_point.pkl")
     except IOError:
         point = pd.read_excel("data/power_osm/raw/power_OSM_combined.xlsx", sheet_name="power_London_point", header=0)
+        point['power'] = point['power'].astype(str)
+        point['name'] = point['name'].astype(str)
+        point['operator'] = point['operator'].astype(str)
         point.to_pickle("data/power_osm/in/power_OSM_point.pkl")
 
     # P.O.1.1.2 Load OSM polygons
@@ -24,6 +27,10 @@ def load_power_osm_data():
         polyg = pd.read_pickle("data/power_osm/in/power_OSM_polygon.pkl")
     except IOError:
         polyg = pd.read_excel("data/power_osm/raw/power_OSM_combined.xlsx", sheet_name="power_London_polygon", header=0)
+        polyg['power'] = polyg['power'].astype(str)
+        polyg['name'] = polyg['name'].astype(str)
+        polyg['substation'] = polyg['substation'].astype(str)
+        polyg['operator'] = polyg['operator'].astype(str)
         polyg.to_pickle("data/power_osm/in/power_OSM_polygon.pkl")
 
     # P.O.1.1.3 Load OSM lines
@@ -31,6 +38,10 @@ def load_power_osm_data():
         line = pd.read_pickle("data/power_osm/in/power_OSM_line.pkl")
     except IOError:
         line = pd.read_excel("data/power_osm/raw/power_OSM_combined.xlsx", sheet_name="power_London_line", header=0)
+        line['power'] = line['power'].astype(str)
+        line['name'] = line['name'].astype(str)
+        line['substation'] = line['substation'].astype(str)
+        line['operator'] = line['operator'].astype(str)
         line.to_pickle("data/power_osm/in/power_OSM_line.pkl")
 
     # P.O.1.1.4 Load OSM line vertices
@@ -38,6 +49,10 @@ def load_power_osm_data():
         linevertex = pd.read_pickle("data/power_osm/in/power_OSM_line_vertices.pkl")
     except IOError:
         linevertex = pd.read_excel("data/power_osm/raw/power_OSM_combined.xlsx", sheet_name="power_London_line_vertices", header=0)
+        linevertex['power'] = linevertex['power'].astype(str)
+        linevertex['name'] = linevertex['name'].astype(str)
+        linevertex['substation'] = linevertex['substation'].astype(str)
+        linevertex['operator'] = linevertex['operator'].astype(str)
         linevertex.to_pickle("data/power_osm/in/power_OSM_line_vertices.pkl")
 
     return point, polyg, line, linevertex
@@ -48,19 +63,15 @@ def create_power_osm_graph(point, polyg, line, linevertex):
 
     G = nx.Graph()
 
-    # P.0.1.2.1 Only extract substations, plants, transformers, poles, towers, and generators > 1 MW from points
+    # P.0.1.2.1 Points
     for _, row in point.iterrows():
-        if row["power"] in ["substation", "sub_station", "plant", "transformer", "pole", "tower"]:
-            G.add_node(row["full_id"], pos=(row["$x"], row["$y"]),
-                       power=row["power"], name=row["name"], operator=row["operator"])
-        elif row["power"] == "generator" and str(row["generator:output:electricity"]).endswith("MW") \
-                and float(str(row["generator:output:electricity"]).replace(" MW", "")) >= 1.0:
+        if row["operator"] == "National Grid":
             G.add_node(row["full_id"], pos=(row["$x"], row["$y"]),
                        power=row["power"], name=row["name"], operator=row["operator"])
 
     # P.0.1.2.2 Extract polygons (exclude relations), but compress them into points based on their centroids
     for _, row in polyg.iterrows():
-        if row["power"] in ["substation", "sub_station", "plant", "transformer"] and row["osm_type"] == "way":
+        if row["power"] == "substation" and ("National Grid" in row["operator"] or row["substation"] == "transmission"):
             G.add_node(row["full_id"], pos=(row["x(centroid($geometry))"], row["y(centroid($geometry))"]),
                        power=row["power"], substation=row["substation"], name=row["name"],
                        operator=row["operator"], voltage=row["voltage"])
@@ -68,7 +79,8 @@ def create_power_osm_graph(point, polyg, line, linevertex):
     # P.0.1.2.3 Some substations, plants and transformers wrongly coded as line
     # -> compress them into points based on their centroids
     for _, row in line.iterrows():
-        if row["power"] in ["substation", "sub_station", "plant", "transformer"]:
+        if row["power"] == "substation" and (row["substation"] in ["transmission", "transition"]
+                                             or "National Grid" in row["operator"]):
             substnnodes = linevertex.loc[linevertex["full_id"] == row["full_id"]]
             x = np.nanmean(substnnodes["$x"])
             y = np.nanmean(substnnodes["$y"])
@@ -78,7 +90,7 @@ def create_power_osm_graph(point, polyg, line, linevertex):
 
     # P.0.1.2.4 Extract cables and lines
     for _, row in line.iterrows():
-        if row["power"] in ["line", "minor_line"]:
+        if row["power"] in ["line"] and ("National Grid" in row["name"] or "National Grid" in row["operator"]):
             # Overhead cables are supported by poles or towers, and assumed to be straight between poles & towers
             # Snap ends to existing substations, generators, transformers, compensators, switches etc.
             linenodes = linevertex.loc[linevertex["full_id"] == row["full_id"]]
@@ -115,7 +127,7 @@ def create_power_osm_graph(point, polyg, line, linevertex):
                 G.add_edge(lnid1, lnid2, power=row["power"], substation=row["substation"], name=row["name"],
                            operator=row["operator"], voltage=row["voltage"])
 
-        elif row["power"] == "cable":
+        elif row["power"] == "cable" and ("National Grid" in row["name"] or "National Grid" in row["operator"]):
             # Underground cables are either linear (deep cable tunnels) or follow along road
             # -> Need to create non-physical nodes for geographical accuracy of intermediate line vertices
             # But simplify the line geometry
@@ -189,7 +201,23 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
 
-    # P.O.4 Add colours and export map plot
+    # P.O.4 Export graph edgelist
+    try:
+        edgelist = pd.DataFrame(columns=["lnid1", "lnid2", "power", "substation", "name", "operator", "voltage"])
+        for u, v in G.edges():
+            series_obj = pd.Series([u, v,
+                                    G.edges[u, v]["power"],
+                                    G.edges[u, v]["substation"],
+                                    G.edges[u, v]["name"],
+                                    G.edges[u, v]["operator"],
+                                    G.edges[u, v]["voltage"]],
+                                   index=edgelist.columns)
+            edgelist = edgelist.append(series_obj, ignore_index=True)
+        edgelist.to_excel(r'data/power_osm/out/power_osm_edgelist.xlsx', index=True)
+    except Exception as e:
+        print(e)
+
+    # P.O.5 Add colours and export map plot
     node_colors = [POWER_COLORS.get(G.nodes[node]["power"], "#000000") for node in G.nodes()]
     edge_powers = nx.get_edge_attributes(G, "power")
     edge_colors = [POWER_COLORS.get(edge_powers[u, v], "#000000") for u, v in G.edges()]
