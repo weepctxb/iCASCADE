9e61d9f6dc11ac188e9d6afe87fe911c22e468ad
EPCTechnologies <weexianbin@yahoo.com.sg>
2021-06-20 23:14:33 +0100

    simplified colours and added colouring for functional states

--- globalparams.py
+++ globalparams.py
@@ -4,7 +4,9 @@ POWER_COLORS = {
     "cable": "#000000",  # 336600
     "line": "#000000",  # #000099
     "minor_line": "#6666ff",
+    "GSP": "#FE2712",
     "substation": "#FE2712",
+    "GSP_transmission": "#990000",
     "substation_transmission": "#990000",
     "substation_traction": "#FF33CC",
     "sub_station": "#FE2712",
@@ -17,6 +19,28 @@ POWER_COLORS = {
     "compensator": "#808080",
     "pole": "#996633",
     "tower": "#604020",
+    "load": "#CC66FF"
+}
+
+POWER_COLORS_MINI = {
+    "cable": "#000000",  # 336600
+    "line": "#000000",  # #000099
+    "minor_line": "#000000",
+    "GSP": "#FE2712",
+    "substation": "#FE2712",
+    "GSP_transmission": "#FE2712",
+    "substation_transmission": "#FE2712",
+    "substation_traction": "#FE2712",
+    "sub_station": "#FE2712",
+    "transformer": "#FE2712",
+    "terminal": "#FE2712",
+    "generator": "#66B032",
+    "plant": "#66B032",
+    "switchgear": "#FE2712",
+    "switch": "#FE2712",
+    "compensator": "#FE2712",
+    "pole": "#996633",
+    "tower": "#996633",
     "load": "#CC66FF"
 }
 
@@ -50,6 +74,43 @@ TRANSPORT_COLORS = {
     "depot": "#FF9900",
     "engine_shed": "#FF9900",
     "signal_box": "#00FFCC"
+}
+
+TRANSPORT_COLORS_MINI = {
+    "bakerloo": "#0009AB",
+    "central": "#0009AB",
+    "circle": "#0009AB",
+    "district": "#0009AB",
+    "hammersmith&city": "#0009AB",
+    "jubilee": "#0009AB",
+    "metropolitan": "#0009AB",
+    "northern": "#0009AB",
+    "piccadilly": "#0009AB",
+    "victoria": "#0009AB",
+    "waterloo&city": "#0009AB",
+    "lo-watform-euston": "#E86A10",
+    "lo-north-london": "#E86A10",
+    "lo-east-london": "#E86A10",
+    "lo-gospeloak-barking": "#E86A10",
+    "dlr": "#00AFAD",
+    "lo-westanglia": "#E86A10",
+    "lo-romford-upminster": "#E86A10",
+    "tfl-rail-shenfield": "#E86A10",
+    "tfl-rail-reading-heathrow": "#E86A10",
+    "trams": "#66CC00",
+    "power_supply": "#FF33CC",
+    "signal": "#00FFCC",
+    "site": "#FF9900",
+    "switch": "#D1D1E0",
+    "yard": "#00FFCC",
+    "depot": "#FF9900",
+    "engine_shed": "#FF9900",
+    "signal_box": "#00FFCC"
+}
+
+STATE_COLORS = {
+    1: "#009900",
+    0: "#FF0000"
 }
 
 LONDON_COORDS = [51.5085300, -0.1257400]
